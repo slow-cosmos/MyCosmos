48e1dc7c1ae031b56b57589212ad424950a4b529
slow-cosmos <inki1030@naver.com>
2022-04-02 19:55:22 +0900

    별 create 카메라 회전 (미완) 2D를 3D로 전환

---
+++ MyCosmos/Assets/Script/Ingame/CreateStar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateStar : MonoBehaviour
+{
+    //https://medium.com/microsoft-design/building-a-virtual-sky-883d4d1080f4
+    public GameObject star;
+    StarDatabase sdb;
+    int maxStar = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sdb = GameObject.Find("StarDatabase").GetComponent<StarDatabase>();
+        Init();
+    }
+
+    void Init()
+    {
+        float x = 0.0f;
+        float y = 0.0f;
+        float z = 0.0f;
+        float ra = 0.0f;
+        float dec = 0.0f;
+        float r = 1000.0f;
+
+        for (int i = 0; i < maxStar; i++)
+        {
+            ra = sdb.star_Database[i].ra;
+            dec = sdb.star_Database[i].dec;
+            SphericalToCartesian(ra, dec, r, ref x, ref y, ref z);
+            Instantiate(star, new Vector3(x, y, z), Quaternion.identity);
+
+        }
+    }
+
+    void SphericalToCartesian (float ra, float dec, float r, ref float x, ref float y, ref float z)
+    {
+        dec = (Mathf.PI / 2) - dec;
+        var rr = r * Mathf.Sin(dec);
+        z = rr * Mathf.Cos(ra);
+        x = rr * Mathf.Sin(ra);
+        y = r * Mathf.Cos(dec);
+    }
+
+}
---
+++ MyCosmos/Assets/Script/Ingame/StarDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarDatabase : MonoBehaviour
+{
+    public struct stars_type
+    {
+        public string name;
+        public float ra, dec, mag;
+        public stars_type(string name, float ra, float dec, float mag)
+        {
+            this.name = name;
+            this.ra = ra;
+            this.dec = dec;
+            this.mag = mag;
+        }
+    }
+
+    public stars_type[] star_Database = new stars_type[]
+    {
+        new stars_type("Sirius", 6.752500f, -16.716101f, -1.460000f)
+
+    };
+}
