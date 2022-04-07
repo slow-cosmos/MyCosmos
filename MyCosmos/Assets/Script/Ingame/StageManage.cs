b0e9a7120e1cecde6ef6b8b335ac8bb81d4ed19a
slow-cosmos <inki1030@naver.com>
2022-04-07 21:08:52 +0900

    Ground 추가 / StageManage 추가

---
+++ MyCosmos/Assets/Script/Ingame/StageManage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChapterManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadSpring();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadSpring()
+    {
+        //땅이랑 카메라 회전(위도, 경도)
+    }
+}
