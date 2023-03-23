f8099a9191f4df05e8ecdc5e0368cd1f1d46c424
slow-cosmos <inki1030@naver.com>
2023-03-24 01:03:55 +0900

    필요 없는 코드 제거

--- MyCosmos/Assets/Script/Lobby/LobbyManage.cs
+++ MyCosmos/Assets/Script/Lobby/LobbyManage.cs
@@ -10,18 +10,10 @@ public class LobbyManage : MonoBehaviour
     [SerializeField]
     Canvas lobbyCanvas;
 
-    [SerializeField]
-    GameObject background;
-
     public void StartButton()
     {
         chapterCanvas.gameObject.SetActive(true);
         lobbyCanvas.gameObject.SetActive(false);
-
-        float initScale = background.transform.localScale.x;
-
-        background.transform.localPosition = new Vector3(-0.08f, 1.01f, 0);
-        background.transform.localScale = new Vector3(initScale*1.33f, initScale * 1.33f, initScale * 1.33f);
 
         SoundManage.instance.PlayButtonSound();
     }
@@ -37,11 +29,6 @@ public class LobbyManage : MonoBehaviour
     {
         chapterCanvas.gameObject.SetActive(false);
         lobbyCanvas.gameObject.SetActive(true);
-
-        float initScale = background.transform.localScale.x;
-
-        background.transform.localPosition = new Vector3(-0.08f, 1.98f, 0);
-        background.transform.localScale = new Vector3(initScale / 1.33f, initScale / 1.33f, initScale / 1.33f);
 
         SoundManage.instance.PlayButtonSound();
     }
